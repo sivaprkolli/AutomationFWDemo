0fc63ec6c6979c3feae350d7f20ccdebcf98776b
Siva <>
2025-02-02 06:13:42 +0530

    auto heal test

---
+++ src/test/java/com/sip/WebTests/AutoHealTest.java
@@ -0,0 +1,44 @@
+package com.sip.WebTests;
+
+import com.epam.healenium.SelfHealingDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//https://github.com/healenium/healenium-example-maven
+public class AutoHealTest {
+    // public static WebDriver selfHealingDriver;
+    // public static SelfHealingDriver driver;
+    static protected SelfHealingDriver driver;
+
+    @Test
+    public void autoHealTest() throws MalformedURLException, InterruptedException {
+        //driver = new ChromeDriver();
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setBrowserName("chrome");
+//        desiredCapabilities.setPlatform(Platform.MAC);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.merge(desiredCapabilities);
+        //WebDriver delegate = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        WebDriver delegate = new ChromeDriver(chromeOptions);
+        driver = SelfHealingDriver.create(delegate);
+
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(5000);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(5000);
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
