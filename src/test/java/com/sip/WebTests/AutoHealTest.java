138cc46ffccdfb4b2f63ebb6737880867d7e2844
Siva <>
2025-05-25 10:14:15 +0530

    self heal test

--- src/test/java/com/sip/WebTests/AutoHealTest.java
+++ src/test/java/com/sip/WebTests/AutoHealTest.java
@@ -2,12 +2,14 @@ package com.sip.WebTests;
 
 import com.epam.healenium.SelfHealingDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -20,6 +22,12 @@ public class AutoHealTest {
     // public static WebDriver selfHealingDriver;
     // public static SelfHealingDriver driver;
     static protected SelfHealingDriver driver;
+
+    @AfterTest
+    public void killSession() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
 
     @Test
     public void autoHealTest() throws MalformedURLException, InterruptedException {
@@ -35,12 +43,15 @@ public class AutoHealTest {
         driver = SelfHealingDriver.create(delegate);
 
         driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
         Thread.sleep(2000);
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        try {
+            driver.findElement(By.xpath("//form/div/input[@placeholder='Username']")).sendKeys("standard_user");
+        }catch (NoSuchElementException nse){
+            driver.findElement(By.id("test1426531")).sendKeys("standard_user");
+        }
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         Thread.sleep(2000);
         driver.findElement(By.id("login-button")).click();
-        Thread.sleep(2000);
-        driver.quit();
     }
 }
